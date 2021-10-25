6cdc56be2fde1140039f38109009d343704d1a71
QuantumaStelata <quantumastelata@gmail.com>
2021-10-26 01:51:08 +0500

    add genre and actor pages

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import MainView, FilmView
+from .views import MainView, FilmView, GenreView, ActorView
 
 urlpatterns = [
     path('', MainView.as_view(), name = 'main'),
     path('film/<str:pk>', FilmView.as_view(), name = 'film'),
+    path('genre/<str:pk>', GenreView.as_view(), name = 'genre'),
+    path('actor/<str:pk>', ActorView.as_view(), name = 'actor'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.shortcuts import get_object_or_404
 
-from apps.database.models import Film
+from apps.database.models import Actor, Film
 from .services import *
 
 class MainView(View):
@@ -15,3 +15,15 @@ class FilmView(View):
     def get(self, request, pk, *args, **kwargs):
         film = get_object_or_404(Film, pk = pk, is_delete = False)
         return render(request, 'main/film.html', {'film': film, **get_recommendations_films(), **get_all_genres()})
+
+class GenreView(View):
+    def get(self, request, pk, *args, **kwargs):
+        genre = get_object_or_404(Genre, pk = pk)
+        films = genre.films.filter(is_delete = False)
+        return render(request, 'main/genre.html', {'genre': genre, 'films': films, **get_recommendations_films(), **get_all_genres()})
+
+class ActorView(View):
+    def get(self, request, pk, *args, **kwargs):
+        actor = get_object_or_404(Actor, pk = pk)
+        films = actor.films.filter(is_delete = False)
+        return render(request, 'main/actor.html', {'actor': actor, 'films': films, **get_recommendations_films(), **get_all_genres()})
